e8c89aa24205505884c6e16cdcb20203d465b223
Alex Pisarik <seo.dnja@gmail.com>
2020-10-02 00:24:54 +0300

    feat: begin vigenere-cipher.js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,52 @@
-const CustomError = require("../extensions/custom-error");
-
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(type = true) {
+    this.typeOfMachine = type;
+    this.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+
+  encrypt(message, key) {
+    if (!message || !key) throw new Error();
+
+    /*TODO: Похоже пробелы не участвуют в построении строки. Нужно сперва убрать пробелы*/
+
+    const localMessage = message.toUpperCase().split('');
+    console.log(localMessage);
+    const messageLength = message.length;
+    console.log(messageLength);
+    const stringToMatch = ''.padEnd(messageLength, key).toUpperCase();
+    const doubleAlphabet = this.alphabet + this.alphabet;
+
+    let result = '';
+
+    for (let i = 0; i < messageLength; ++i) {
+      const messageChar = localMessage[i];
+      const secretChar = stringToMatch[i];
+
+      if (!/[A-Z]/.test(messageChar))
+      {
+        result += messageChar;
+      } else {
+
+        const positionMessageChar = messageChar.charCodeAt(0) - 'A'.charCodeAt(0);
+        const positionSecretChar = secretChar.charCodeAt(0) - 'A'.charCodeAt(0);
+        // console.log('positionSecretChar', positionSecretChar);
+        // console.log('positionMessageChar', positionMessageChar);
+
+        const resultCharPosition = positionSecretChar + positionMessageChar;
+
+        const resultChar = doubleAlphabet.charAt(resultCharPosition);
+        // console.log(resultChar);
+
+        result += resultChar;
+      }
+      console.log(result);
+    }
+  }
+
+
+  decrypt(message, key) {
+    if (!message || !key) throw new Error();
+
   }
 }
 
