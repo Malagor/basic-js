b54107cd12d4c27977d6fdba7fd09ef476d375b0
Alex Pisarik <seo.dnja@gmail.com>
2020-10-04 21:27:42 +0300

    refactor: what-season.js

--- src/what-season.js
+++ src/what-season.js
@@ -2,36 +2,10 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
 
-  // throw new CustomError('Not implemented');
-
-  // if (arguments[0] === undefined || !date || (date - new Date()) === 0) {
-  //   throw new Error('Unable to determine the time of year!');
-  // }
-  //
-  // if (typeof date.getMonth !== 'function' && typeof date.getMonth !== 'function' && date instanceof Date) {
-  //   throw new Error('Error');
-  // }
-  //
-  // if (date.__proto__.__proto__ === null){
-  //   throw new Error('THROWN');
-  // }
-  //
-  // if (!(date instanceof Date) && isNaN(date.getMonth)) {
-  //   throw new Error('Error')
-  // }
-  //
-  // if (arguments[0] === undefined || !date || (date - new Date()) === 0) {
-  //   throw new Error('Unable to determine the time of year!');
-  // }
-  // if (!(date instanceof Date) && isNaN(date.getMonth)) {
-  //   throw new Error('Error');
-  // }
-
-  // if (arguments[0] === undefined) {
-  if (!arguments[0]) {
+  if (!date) {
     return 'Unable to determine the time of year!';
   }
-  if (!(date instanceof Date) && isNaN(date.getMonth) || !date || (date - new Date()) === 0) {
+  if (!(date instanceof Date) || (date - new Date()) === 0) {
     throw new Error('Error');
   }
 
