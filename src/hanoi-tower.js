26852bf97013aaa62e1bd7643fb710b5c08759ac
Alex Pisarik <seo.dnja@gmail.com>
2020-10-04 21:20:46 +0300

    refactor: hanoi-tower.js

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -2,7 +2,7 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function calculateHanoi(disksNumber, turnsSpeed ) {
   const turns = ( 2 ** disksNumber - 1 );
-  let sec = Math.floor(turns / turnsSpeed * 3600);
+  const sec = Math.floor(turns / turnsSpeed * 3600);
 
   return {
     turns: turns,
